cfa973bb5f4d90366cf2b5fb0282490a93875aa3
Romeo C. Rubio III <romeocrubioiii@gmail.com>
2024-06-12 19:37:13 +0800

    Added biometrics functionality

--- app/src/main/java/com/rubio/pupad/LoginActivity.java
+++ app/src/main/java/com/rubio/pupad/LoginActivity.java
@@ -1,17 +1,25 @@
 package com.rubio.pupad;
 
+import static androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG;
+import static androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -21,12 +29,25 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+import java.util.concurrent.Executor;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 101010;
     EditText emailEditText, passwordEditText;
     Button loginBtn;
     ProgressBar progressBar;
     TextView createAccountBtnTextView;
+    Button biometricsBtn;
+    SharedPreferences sharedPreferences;
+
+
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +60,93 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn = findViewById(R.id.login_btn);
         progressBar = findViewById(R.id.progrss_bar);
         createAccountBtnTextView = findViewById(R.id.create_account_text_view_btn);
+        biometricsBtn = findViewById(R.id.biometrics_btn);
 
         loginBtn.setOnClickListener((v)-> loginUser());
         createAccountBtnTextView.setOnClickListener((v)->startActivity(new Intent(LoginActivity.this,CreateAccountActivity.class)));
 
+        sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
+        boolean isLogin=sharedPreferences.getBoolean("isLogin",false);
+        if (isLogin)
+        {
+
+            biometricsBtn.setVisibility(View.VISIBLE);
+        }
+
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Log.d("MY_APP_TAG", "App can authenticate using biometrics.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(this,"Biometrics sensor doesn't exist", Toast.LENGTH_SHORT).show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(this,"Biometrics sensor not available", Toast.LENGTH_SHORT).show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                // Prompts the user to create credentials that your app accepts.
+                final Intent enrollIntent = new Intent(Settings.ACTION_BIOMETRIC_ENROLL);
+                enrollIntent.putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                        BIOMETRIC_STRONG | DEVICE_CREDENTIAL);
+                startActivityForResult(enrollIntent, REQUEST_CODE);
+                break;
+        }
+
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(LoginActivity.this,
+                executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                                "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+
+                String email=sharedPreferences.getString("email","");
+                String password=sharedPreferences.getString("password","");
+
+                Log.d("MY_APP_TAG", "Retrieved email: " + email);
+                Log.d("MY_APP_TAG", "Retrieved password: " + password);
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Stored credentials are empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                loginAccountInFirebase(email, password);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication failed",
+                                Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for my app")
+                .setSubtitle("Log in using your biometric credential")
+                .setNegativeButtonText("Use account password")
+                .build();
+
+
+        biometricsBtn.setOnClickListener(view -> {
+            biometricPrompt.authenticate(promptInfo);
+        });
     }
+
+
+
 
     void loginUser(){
         String email = emailEditText.getText().toString();
@@ -67,9 +170,14 @@ public class LoginActivity extends AppCompatActivity {
                 changeInProgress(false);
                 if (task.isSuccessful()){
                     if (firebaseAuth.getCurrentUser().isEmailVerified()){
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putBoolean("isLogin", true);
+                        editor.putString("email", email);
+                        editor.putString("password", password);
+                        editor.apply();
+
                         startActivity(new Intent(LoginActivity.this,ewan.class));
                         finish();
-
                     }else {
                         Utility.showToast(LoginActivity.this,"Email not verified. Please verify your email first.");
                     }
